efa2c898e17b46baa6f5fe7f67422585f3fcfccd
DianaU21 <163894805+DianaU21@users.noreply.github.com>
2024-04-08 19:43:42 +0300

    v3
    3

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -9,8 +9,8 @@ public class Test extends BaseTest {
     public void registerNewAccount() throws InterruptedException {
         RegisterAccount registerAccount = new RegisterAccount(driver);
         registerAccount.clickRegisterButton();
+        registerAccount.registerAccountInfo("dianaungureanu12@gmail.com","floridelivrat1#","floridelivrat1#");
         Thread.sleep(2000);
-        registerAccount.registerAccountInfo("dianaungureanu7@gmail.com","floridelivrat1#","floridelivrat1#");
         registerAccount.clickRegisterAccount();
         registerAccount.clickEnterTheAccount();
         Thread.sleep(5000);
@@ -20,8 +20,8 @@ public class Test extends BaseTest {
     public void loginNewAccount() throws InterruptedException {
         LoginAccount loginAccount= new LoginAccount(driver);
         loginAccount.clickLoginButton();
-        Thread.sleep(2000);
         loginAccount.loginAccountInfo("dianaungureanu2@gmail.com","floridelivrat1#");
+        Thread.sleep(2000);
         loginAccount.selectKeepMeLoggedIn();
         loginAccount.clickLoginAccount();
         Thread.sleep(5000);
@@ -31,8 +31,8 @@ public class Test extends BaseTest {
     public void verifyRegisterErrorWithAnExistingEmail() throws InterruptedException{
         RegisterExistingEmail registerExistingEmail=new RegisterExistingEmail(driver);
         registerExistingEmail.clickRegisterButton();
-        Thread.sleep(2000);
         registerExistingEmail.registerAccountInfoSameEmail("dianaungureanu2@gmail.com","floridelivrat1#","floridelivrat1#");
+        Thread.sleep(2000);
         registerExistingEmail.clickRegisterAccount();
         registerExistingEmail.verifyErrorMessageWithEmailAlredyRegistred();
         Thread.sleep(5000);
@@ -42,8 +42,8 @@ public class Test extends BaseTest {
     public void verifySearchSpecificProductAndViewDetails() throws InterruptedException{
         SearchProduct searchProduct= new SearchProduct(driver);
         searchProduct.specificSearchProduct("bambus");
-        Thread.sleep(2000);
         searchProduct.clickSearch();
+        Thread.sleep(2000);
         searchProduct.clickViewProduct();
         Thread.sleep(5000);
     }
